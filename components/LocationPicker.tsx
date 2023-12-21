4a551c12403aa48706116f3c8118b9a4a573849f
varun-balani <varunprahladb@gmail.com>
2023-12-22 00:53:06 +0530

    revert to select

--- components/LocationPicker.tsx
+++ components/LocationPicker.tsx
@@ -11,29 +11,11 @@ import {
     SelectValue,
 } from "@/components/ui/select";
 import useSWR from "swr";
-import { cn } from "@/lib/utils";
-import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons";
-import { Button } from "@/components/ui/button";
-import {
-    Command,
-    CommandEmpty,
-    CommandGroup,
-    CommandInput,
-    CommandItem,
-} from "@/components/ui/command";
-import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "@/components/ui/popover";
 
 //@ts-ignore
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function LocationPicker() {
-    const [open, setOpen] = React.useState(false);
-    const [value, setValue] = React.useState("");
-
     const { data, error } = useSWR(
         "https://restcountries.com/v3.1/all?fields=name,capitalInfo",
         fetcher
@@ -41,7 +23,7 @@ export default function LocationPicker() {
     if (error) return <div>Failed to load countries.</div>;
 
     const handleChange = (value: any) => {
-        if (value === "my current location") {
+        if (value === "Current Location") {
             if ("geolocation" in navigator) {
                 // Retrieve latitude & longitude coordinates from `navigator.geolocation` Web API
                 navigator.geolocation.getCurrentPosition(({ coords }) => {
@@ -56,81 +38,49 @@ export default function LocationPicker() {
         } else {
             localStorage.setItem("location", value);
             const country = data.find((country: any) => {
-                return country.name.common.toLowerCase() === value;
+                return country.name.common === value;
             });
             localStorage.setItem("latitude", country.capitalInfo.latlng[0]);
             localStorage.setItem("longitude", country.capitalInfo.latlng[1]);
+            console.log(localStorage.getItem("latitude"));
+            console.log(localStorage.getItem("longitude"));
         }
     };
 
     return (
-        <Popover open={open} onOpenChange={setOpen}>
-            <PopoverTrigger asChild>
-                <Button
-                    variant="outline"
-                    role="combobox"
-                    aria-expanded={open}
-                    className="w-[200px] justify-between"
-                >
-                    {localStorage.getItem("location")
-                        ? localStorage.getItem("location")
-                        : "Choose Location"}
-                    <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-[200px] p-0">
-                <Command>
-                    <CommandInput
-                        placeholder="Search location..."
-                        className="h-9"
-                    />
-                    <CommandEmpty>No location found.</CommandEmpty>
-                    <CommandGroup>
-                        {!data && (
-                            <SelectItem value="loading">Loading...</SelectItem>
-                        )}
-                        <CommandItem
-                            value="My Current Location"
+        <Select onValueChange={handleChange}>
+            <SelectTrigger className="w-[200px]">
+                <SelectValue
+                    placeholder={
+                        localStorage.getItem("location")
+                            ? localStorage.getItem("location")
+                            : "Choose Location"
+                    }
+                />
+            </SelectTrigger>
+            <SelectContent>
+                {!data && <SelectItem value="loading">Loading...</SelectItem>}
+                {data && (
+                    <SelectGroup>
+                        <SelectItem
+                            value="Current Location"
                             className="font-bold"
-                            onSelect={(currentValue) => {
-                                setValue(
-                                    currentValue === value ? "" : currentValue
-                                );
-                                setOpen(false);
-                                handleChange(currentValue);
-                            }}
                         >
                             My Current Location
-                        </CommandItem>
-                        {data &&
-                            data.map((country: any) => (
-                                <CommandItem
-                                    key={country.name.common}
+                        </SelectItem>
+                        {data.map((country: any) => {
+                            return (
+                                <SelectItem
                                     value={country.name.common}
-                                    onSelect={(currentValue) => {
-                                        setValue(
-                                            currentValue === value
-                                                ? ""
-                                                : currentValue
-                                        );
-                                        setOpen(false);
-                                        handleChange(currentValue);
-                                    }}
+                                    key={country.name.common}
                                 >
                                     {country.name.common}
-                                    <CheckIcon
-                                        className={cn(
-                                            "ml-auto h-4 w-4",
-                                            value === country.name.common
-                                                ? "opacity-100"
-                                                : "opacity-0"
-                                        )}
-                                    />
-                                </CommandItem>
-                            ))}
-                    </CommandGroup>
-                </Command>
-            </PopoverContent>
-        </Popover>
+                                </SelectItem>
+                            );
+                        })}
+                    </SelectGroup>
+                )}
+            </SelectContent>
+        </Select>
     );
 }
